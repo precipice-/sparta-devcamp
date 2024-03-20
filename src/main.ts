5f0586faae72d80e45d426d7ca696d18f4b95191
choisooyoung-dev <sooyoung.choi.dev@gmail.com>
2024-03-21 01:12:02 +0900

    init: 초기 환경변수 설정

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  const configService = app.get(ConfigService);
+  const port = configService.get<number>('PORT');
+  const env = configService.get<string>('RUNTIME');
+  const serviceName = configService.get<string>('SERVICE_NAME');
+  console.log(`runtime: ${env}\tport: ${port}\tserviceName: ${serviceName}`);
+  await app.listen(port);
 }
 bootstrap();
